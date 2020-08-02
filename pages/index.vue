847edaa75bade7e3c7ea1601ef6516402a06091b
juntaro0202 <zyunn14641@gmail.com>
2020-08-02 17:00:35 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <youtube width="300" height="200" :video-id="videoId" />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import VueYoutube from "vue-youtube";
+
+Vue.use(VueYoutube);
+
+export default {
+  data() {
+    return {
+      videoId: "fHuO3Xaje98"
+    };
+  }
+};
+</script>
---
+++ pages/toyota.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <youtube width="800" height="400" :video-id="videoId" />
+    </v-flex>
+    <!-- 追加 -->
+    <p>
+      <v-btn class="ma-2" text icon color="blue lighten-2">
+        <v-icon>mdi-thumb-up</v-icon>
+      </v-btn>
+
+      <v-btn class="ma-2" text icon color="red lighten-2">
+        <v-icon>mdi-thumb-down</v-icon>
+      </v-btn>
+    </p>
+  </v-layout>
+</template>
+
+<script>
+import Vue from "vue";
+import VueYoutube from "vue-youtube";
+
+Vue.use(VueYoutube);
+
+export default {
+  data() {
+    return {
+      videoId: "fExYWri0-fs"
+    };
+  }
+};
+</script>
